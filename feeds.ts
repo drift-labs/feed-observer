964e391e82630ab9df05fe694f4ea742665da3fb
Chris Heaney <chrisheaney30@gmail.com>
2022-05-05 15:50:11 -0700

    add GMT and CRV oracles

--- feeds.ts
+++ feeds.ts
@@ -1,4 +1,6 @@
 export const FEEDS = [
 	'WMW5xc3HypXwTnPesyUT49uLsyHwNURsWAEk39onKuk', // XRP
-	'74zeQpprjNtEghGiC3VEPsR9y4kR2GTd4Rq9YVk9tnjz' // APE
+	'74zeQpprjNtEghGiC3VEPsR9y4kR2GTd4Rq9YVk9tnjz', // APE
+	'81kbdwDfTNsg4d5D1BxE9FHEdxEMk8xCJUj5ujjWQyvd', // GMT
+	'61NzdQ9y6KhMhWxyJ8kXEe25NxdixVL1YUnGYLDMaUxH', // CRV
 ];
